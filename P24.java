e8059d41a73ef8adb6eab1d881a03b02e17b79e8
dalip8797 <dalip8797@gmail.com>
2018-06-15 20:32:24 +0530

    added p23 file

---
+++ P24.java
@@ -0,0 +1,20 @@
+package program19;
+
+/**
+ *
+ * @author User
+ */
+public class Program19 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=Integer.parseInt(args[0]);
+       int b =Integer.parseInt(args[1]);
+        System.out.println("sum is="+(a+b));
+        // TODO code application logic here
+    }
+    
+}
+
---
+++ p23.java
@@ -0,0 +1,24 @@
+package javaapplication29;
+public interface NewInterface {
+    public void dalip();
+    public void cat();
+    
+}
+package javaapplication29;
+
+public class derive {
+    public void dalip()
+    {
+        System.out.println("i am in dalip function");
+       
+    }
+    public void cat()
+    {
+        System.out.println(" i am in cat function");
+    }
+    
+     
+            
+    
+    
+}
